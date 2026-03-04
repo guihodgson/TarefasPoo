007b1d97d6e5ce2fdcfb34efa4e76584f5f0d2f1
guihodgson <guihodgson@hotmail.com>
2026-03-04 17:23:53 -0300

    Fiz classe heroi

---
+++ Tarefa1/src/Heroi.java
@@ -0,0 +1,79 @@
+public class Heroi {
+    private String nome;
+    private int vidaMax;
+    private int vida;
+    private int escudo;
+
+    // Getters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getVidaMax() {
+        return vidaMax;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public int getEscudo() {
+        return escudo;
+    }
+
+
+    // Constructor
+
+    public Heroi(String nome, int vida, int escudo) {
+        this.nome = nome;
+        this.vida = vidaMax = vida;
+        this.escudo = escudo;
+    }
+
+    // Metodos
+
+    public int receberDano(int dano) {
+        if (dano <= escudo) {
+            return vida;
+        }
+        else {
+            vida -= (dano - escudo);
+            return vida;
+        }
+    }
+
+    public int curarVida(int valor) {
+        if (vida + valor > vidaMax) {
+            vida = vidaMax;
+        }
+        else {
+            vida += valor;
+        }
+        return vida;
+    }
+
+    public int ganharEscudo(int valor) {
+        escudo += valor;
+        return escudo;
+    }
+
+    public int perderEscudo(int valor) {
+        if (escudo - valor < 0) {
+            escudo = 0;
+        }
+        else {
+            escudo -= valor;
+        }
+        return escudo;
+    }
+
+    public boolean estaVivo() {
+        if (vida > 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
