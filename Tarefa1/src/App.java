7cbc7117d692dea1b80699cfe76237bc1d328158
Guiherme Boarini Hodgson <218323@dac.unicamp.br>
2026-03-05 13:42:27 -0300

    terminei basico da main e input handler

--- Tarefa1/src/App.java
+++ Tarefa1/src/App.java
@@ -1,8 +1,16 @@
+
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Heroi heroi = new Heroi("Po, o Dragao Guerreiro", 40, 0, 4);
         Inimigo loboCapanga = new Inimigo("Lobo Solitario", 30, 0, 6);
         InputHandler inputHandler = new InputHandler();
+        ArrayList<String> opcoesCartas = new ArrayList<>();
+        opcoesCartas.add("Posicao 0 invalida");
+        opcoesCartas.add("Carta Dano");
+        opcoesCartas.add("Carta Escudo");
+        opcoesCartas.add("Encerrar Turno");
 
         do {
             System.out.println(heroi.getNome() + ": (" + heroi.getVida() + "/" + heroi.getVidaMax() + " HP) (" + heroi.getEscudo() + " de escudo.)");
@@ -12,6 +20,31 @@ public class App {
 
             System.out.println(heroi.getEnergia() + "/" + heroi.getEnergiaMax() + " de Energia restantes.");
 
-        } while (heroi.estaVivo() && Inimigo.estaVivo());
+            int opcao = inputHandler.selecionar(opcoesCartas);
+
+            if (opcao == 1) {
+                if (heroi.podeGastarEnergia(1)) {
+                    // Colocar ataque da cartaDano
+                }
+                else {
+                    System.out.println("Energia insuficiente.");
+                }
+                continue;
+            }
+            if (opcao == 2) {
+                if (heroi.podeGastarEnergia(1)) {
+                    // Colocar escudo da cartaEscudo
+                }
+                else {
+                    System.out.println("Energia insuficiente.");
+                }
+                continue;
+            }
+            if (opcao == 3) {
+                loboCapanga.atacar(heroi);
+                heroi.resetRound();
+            }
+
+        } while (heroi.estaVivo() && loboCapanga.estaVivo());
     }
 }
